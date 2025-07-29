edf69de61e5bcbc07ad367ad076e9d085e4968d8
jim <>
2025-07-29 13:51:25 +0800

    調整日期功能

--- src/main/java/com/qiyuan/web/dto/request/QueryOrderRequest.java
+++ src/main/java/com/qiyuan/web/dto/request/QueryOrderRequest.java
@@ -7,13 +7,17 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
 public class QueryOrderRequest {
     private String status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
     private Date startTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
     private Date endTime;
     private Integer page;
     private Integer size;
