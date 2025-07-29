429c2dd007f5beb5052ecd9023d76ffb593293f9
chloe <test@test.com>
2025-07-29 23:32:22 +0800

    金流報表新增欄位

--- src/main/java/com/qiyuan/web/dto/MoneyReportVO.java
+++ src/main/java/com/qiyuan/web/dto/MoneyReportVO.java
@@ -17,10 +17,13 @@ public class MoneyReportVO {
     @Schema(description = "UUID", example = "a1b2c3d4e5f6...")
     private String id;
 
-    @Schema(description = "外部訂單編號")
+    @Schema(description = "對外訂單編號")
     private String externalOrderNo;
 
-    @Schema(description = "類型 (O:供品, M:老師, G:請神, L:點燈)", example = "O")
+    @Schema(description = "第三方訂單編號")
+    private String providerOrderNo;
+
+    @Schema(description = "類型 (O:供品, M:老師, G:請神, L:點燈, R: 實體商品)", example = "O")
     private String type;
 
     @Schema(description = "付款狀態 (created/paid/...)", example = "paid")
